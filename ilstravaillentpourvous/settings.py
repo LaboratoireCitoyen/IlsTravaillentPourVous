004129c4e23462d934ab59bdd6dfee8e86c06141
jpic <jamespic@gmail.com>
2015-01-12 15:47:09 +0100

    Removed duplicate setting

--- ilstravaillentpourvous/settings.py
+++ ilstravaillentpourvous/settings.py
@@ -110,17 +110,6 @@ STATICFILES_FINDERS += (
     'pipeline.finders.PipelineFinder',
 )
 
-
-# Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 DATABASES = {
     'default': {
         'ENGINE': os.environ.get('DB_ENGINE',
